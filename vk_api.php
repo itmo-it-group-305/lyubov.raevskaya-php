2c8ed95393d351cdbb816d46f7f83fe0a9adc6ca
Lyubov Raevskaya <lubraevskaya@gmail.com>
2016-01-18 00:03:43 +0300

    не работает 3

--- vk_api.php
+++ vk_api.php
@@ -12,14 +12,15 @@ $data = array(
     'user_id' => 689643,
     'fields' => 'bdate,city,photo_50,online',
     'count' => 20,
+    'v' => 5.44,
     );
 //user_id=689643&fields=nickname,city,photo_50,online';
 $content = $url.http_build_query($data);
 $get = file_get_contents($content);
 echo $get;
-$friends = json_decode($get);
-$friends['response'][0];
-$post = true;
+$friends = json_decode($get, true);
+$friends['response'][0]['uid'];
+//$post = true;
 //echo $friends;
 ?>
 
@@ -33,13 +34,16 @@ $post = true;
 
 <?php
 foreach ($friends as $post): ?>
+
     <section>
         <h2>My friends:</h2>
         <img src="<?= $post['photo_50'] ?>">
-        <p> <?= $post['first_name'.'last_name']?></p>
+        <p> <?= $post['first_name']?></p>
+        <p> <?= $post['last_name']?></p>
+        <p> <?= $post['bdate']?></p>
         <p><?= $post['city']?></p>
-
     </section>
+
 <?php endforeach; ?>
 
 </body>
